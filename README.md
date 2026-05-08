3d177c6c9be1c8ea79a04d6dfe4fe8a5532f18d1
Pranjal patel <prannjalpatel8827@gmail.com>
2026-05-07 23:51:49 -0700

    interview-maker project

--- README.md
+++
@@ -1,2 +0,0 @@
-It is a genai project in which user uploads resume or writes self description and a target job description.
-it generates a matched score and some technical questions. 7 days roadmap for you prepration also tells the skill gaps
---
+++ server/src/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cookieParser from "cookie-parser"
+import cors from "cors"
+
+const app = express()
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({
+    origin: process.env.CLIENT_BASE_URL,
+    credentials: true
+}))
+
+/* require all the routes here */
+import authRouter from "./routes/auth.routes.js"
+import interviewRouter from "./routes/interview.routes.js"
+
+
+/* using all the routes here */
+app.use("/api/auth", authRouter)
+app.use("/api/interview", interviewRouter)
+
+
+
+export default app
